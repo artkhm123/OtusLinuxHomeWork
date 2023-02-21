89d3338e72d73e5441b6eb455bd9b880c3b83fcf
Artem Khomyakov <khomyakov-etx@yandex.ru>
2023-02-21 09:26:57 +0300

    code style fixed

--- parser.py
+++ parser.py
@@ -1,14 +1,17 @@
-from subprocess import *
-import sys
 import datetime
+import sys
+from subprocess import *
+
 
 def get_timedate():
     date_time = datetime.datetime.now()
     return date_time.strftime("%d-%m-%Y-%H-%M-scan.txt")
 
+
 def open_file(file_name):
     result = run(["cat", f"{file_name}"], stdout=PIPE, encoding='utf-8').stdout
     print(result)
+
 
 def get_data_ps_aux():
     raw_data_by_lines = Popen(['ps', 'aux'], stdout=PIPE, encoding='utf-8').stdout.readlines()
@@ -16,6 +19,7 @@ def get_data_ps_aux():
     fullfilment = map(lambda line: line.strip().split(None, len(headers) - 1), raw_data_by_lines[1:])
     data_dict = [dict(zip(headers, line)) for line in fullfilment]
     return data_dict
+
 
 def parce_to_file(file_name):
     data = get_data_ps_aux()
@@ -26,7 +30,7 @@ def parce_to_file(file_name):
     minRSS = int(data[0]["RSS"])
     maxCPU = float(data[0]["%CPU"])
     minCPU = float(data[0]["%CPU"])
-    user_proccesses_dict = {data[0]["USER"]:0}
+    user_proccesses_dict = {data[0]["USER"]: 0}
     for i in data:
         users.add(i["USER"])
         cpu.append(float(i["%CPU"]))
@@ -53,14 +57,16 @@ def parce_to_file(file_name):
         print("Отчёт о состоянии системы:")
         print("Пользователи системы: ", ', '.join(map(str, users)))
         print("Всего процессов запущено:", len(data))
-        print("Пользовательские процессы:", str(user_proccesses_dict).replace("{","").replace("'","").replace("}",""))
-        print("Всего памяти используется:", sum(mem) , "b")
+        print("Пользовательские процессы:",
+              str(user_proccesses_dict).replace("{", "").replace("'", "").replace("}", ""))
+        print("Всего памяти используется:", sum(mem), "b")
         print("Всего CPU используется:", sum(cpu), "%")
         print("Больше всего памяти использует:", maxRSS_proces["COMMAND"][:20])
         print("Меньше всего памяти использует:", minRSS_proces["COMMAND"][:20])
         print("Больше всего CPU использует:", maxRSS_proces["COMMAND"][:20])
         print("Меньше всего CPU использует:", minRSS_proces["COMMAND"][:20])
     sys.stdout = sys.__stdout__
+
 
 if __name__ == '__main__':
     file_name_ = get_timedate()
